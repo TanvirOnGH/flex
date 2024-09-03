ccbcb51ebb9beff807cf14ab1f152b0894ce3924
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-03 15:32:53 +0000

    format lua code with stylua
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- widget/tabbar/boxes.lua
+++ widget/tabbar/boxes.lua
@@ -18,41 +18,41 @@ local size = beautiful.tabbar_size or 40
 local position = beautiful.tabbar_position or "bottom"
 
 local function create(c, focused_bool, buttons, inactive_bool)
-    local bg_temp = inactive_bool and bg_normal_inactive or bg_normal
-    local fg_temp = inactive_bool and fg_normal_inactive or fg_normal
-    if focused_bool then
-        bg_temp = inactive_bool and bg_focus_inactive or bg_focus
-        fg_temp = inactive_bool and fg_focus_inactive or fg_focus
-    end
-    local wid_temp = wibox.widget({
-        {
-            {
-                awful.widget.clienticon(c),
-                left = 10,
-                right = 10,
-                bottom = 10,
-                top = 10,
-                widget = wibox.container.margin(),
-            },
-            widget = wibox.container.place(),
-        },
-        buttons = buttons,
-        bg = bg_temp,
-        widget = wibox.container.background(),
-    })
-    return wid_temp
+	local bg_temp = inactive_bool and bg_normal_inactive or bg_normal
+	local fg_temp = inactive_bool and fg_normal_inactive or fg_normal
+	if focused_bool then
+		bg_temp = inactive_bool and bg_focus_inactive or bg_focus
+		fg_temp = inactive_bool and fg_focus_inactive or fg_focus
+	end
+	local wid_temp = wibox.widget({
+		{
+			{
+				awful.widget.clienticon(c),
+				left = 10,
+				right = 10,
+				bottom = 10,
+				top = 10,
+				widget = wibox.container.margin(),
+			},
+			widget = wibox.container.place(),
+		},
+		buttons = buttons,
+		bg = bg_temp,
+		widget = wibox.container.background(),
+	})
+	return wid_temp
 end
 
 local layout = wibox.layout.fixed.horizontal
 if position == "left" or position == "right" then
-    layout = wibox.layout.fixed.vertical
+	layout = wibox.layout.fixed.vertical
 end
 
 return {
-    layout = layout,
-    create = create,
-    position = position,
-    size = size,
-    bg_normal = bg_normal,
-    bg_focus = bg_normal,
+	layout = layout,
+	create = create,
+	position = position,
+	size = size,
+	bg_normal = bg_normal,
+	bg_focus = bg_normal,
 }
--- widget/tabbar/pure.lua
+++ widget/tabbar/pure.lua
@@ -18,65 +18,49 @@ local size = beautiful.tabbar_size or 20
 local position = beautiful.tabbar_position or "top"
 
 local function create(c, focused_bool, buttons, inactive_bool)
-    local bg_temp = inactive_bool and bg_normal_inactive or bg_normal
-    local fg_temp = inactive_bool and fg_normal_inactive or fg_normal
-    if focused_bool then
-        bg_temp = inactive_bool and bg_focus_inactive or bg_focus
-        fg_temp = inactive_bool and fg_focus_inactive or fg_focus
-    end
+	local bg_temp = inactive_bool and bg_normal_inactive or bg_normal
+	local fg_temp = inactive_bool and fg_normal_inactive or fg_normal
+	if focused_bool then
+		bg_temp = inactive_bool and bg_focus_inactive or bg_focus
+		fg_temp = inactive_bool and fg_focus_inactive or fg_focus
+	end
 
-    local wid_temp = wibox.widget({
-        {
-            { -- Left
-                wibox.widget.base.make_widget(
-                    awful.titlebar.widget.iconwidget(c)
-                ),
-                buttons = buttons,
-                layout = wibox.layout.fixed.horizontal,
-            },
-            { -- Title
-                wibox.widget.base.make_widget(
-                    awful.titlebar.widget.titlewidget(c)
-                ),
-                buttons = buttons,
-                widget = wibox.container.place,
-            },
-            { -- Right
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.floatingbutton(c)
-                ) or nil,
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.stickybutton(c)
-                ) or nil,
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.ontopbutton(c)
-                ) or nil,
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.maximizedbutton(c)
-                ) or nil,
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.minimizebutton(c)
-                ) or nil,
-                focused_bool and wibox.widget.base.make_widget(
-                    awful.titlebar.widget.closebutton(c)
-                ) or nil,
-                layout = wibox.layout.fixed.horizontal,
-            },
-            layout = wibox.layout.align.horizontal,
-        },
-        bg = bg_temp,
-        fg = fg_temp,
-        widget = wibox.container.background,
-    })
+	local wid_temp = wibox.widget({
+		{
+			{ -- Left
+				wibox.widget.base.make_widget(awful.titlebar.widget.iconwidget(c)),
+				buttons = buttons,
+				layout = wibox.layout.fixed.horizontal,
+			},
+			{ -- Title
+				wibox.widget.base.make_widget(awful.titlebar.widget.titlewidget(c)),
+				buttons = buttons,
+				widget = wibox.container.place,
+			},
+			{ -- Right
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.floatingbutton(c)) or nil,
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.stickybutton(c)) or nil,
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.ontopbutton(c)) or nil,
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.maximizedbutton(c)) or nil,
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.minimizebutton(c)) or nil,
+				focused_bool and wibox.widget.base.make_widget(awful.titlebar.widget.closebutton(c)) or nil,
+				layout = wibox.layout.fixed.horizontal,
+			},
+			layout = wibox.layout.align.horizontal,
+		},
+		bg = bg_temp,
+		fg = fg_temp,
+		widget = wibox.container.background,
+	})
 
-    return wid_temp
+	return wid_temp
 end
 
 return {
-    layout = wibox.layout.flex.horizontal,
-    create = create,
-    position = position,
-    size = size,
-    bg_normal = bg_normal,
-    bg_focus = bg_focus,
+	layout = wibox.layout.flex.horizontal,
+	create = create,
+	position = position,
+	size = size,
+	bg_normal = bg_normal,
+	bg_focus = bg_focus,
 }
